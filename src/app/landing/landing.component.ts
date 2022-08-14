9e7d7fe931605866887273d9a745e5eb59182ba3
sdnyanesh352 <sdnyanesh352@gmail.com>
2022-08-14 16:33:01 +0530

    ok

---
+++ src/app/landing/landing.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing',
+  templateUrl: './landing.component.html',
+  styleUrls: ['./landing.component.scss']
+})
+export class LandingComponent implements OnInit {
+
+  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.slides[0] = {
+      id: 0,
+      src: './assets/img/banner1.jpg',
+      title: 'First slide',
+      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
+    };
+    this.slides[1] = {
+      id: 1,
+      src: './assets/img/banner1.jpg',
+      title: 'Second slide',
+      subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+    }
+    this.slides[2] = {
+      id: 2,
+      src: './assets/img/banner1.jpg',
+      title: 'Third slide',
+      subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
+    }
+  }
+
+}
